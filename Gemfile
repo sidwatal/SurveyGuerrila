6a952850df252d1a0243a507d3d48f2cbe69cfc0
ScottBWar <scottbwarner@gmail.com>
2015-05-08 17:11:57 -0400

    added authentication

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'rake'
 
 gem 'shotgun'
 
+gem 'bcrypt'
+
 group :test do
   gem 'shoulda-matchers'
   gem 'rack-test'
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,4 +1,45 @@
-get '/dashboard' do
-  erb :show
+
+get '/signup' do
+
+
+  erb :'users/signup'
 end
+
+
+
+
+post '/signup' do
+  user = User.new(name: params[:name], email: params[:email], password: params[:password])
+  if user.save
+    session[:user_id] = user.id
+  redirect '/dashboard'
+  end
+end
+
+
+post '/login' do
+  current_user = User.find_by(email: params[:email])
+  if current_user && current_user.authenticate(params[:password])
+    session[:user_id] = current_user.id
+    redirect '/dashboard'
+  else
+    p "unauthorized log-in attempt"
+    redirect '/'
+  end
+end
+
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+
+
+get '/dashboard' do
+
+erb :'/users/show'
+end
+
+
 
