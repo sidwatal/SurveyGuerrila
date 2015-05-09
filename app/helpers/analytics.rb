724e4d97704aa59fec7b34e522364ac4c1c32600
Sid Watal <sid.watal@gmail.com>
2015-05-09 16:10:17 -0400

    add bardata array to analytics

--- app/helpers/analytics.rb
+++ app/helpers/analytics.rb
@@ -1,5 +1,5 @@
 def response_percentage(choice, survey)
   numerator = choice.responses.count
   denominator = survey.surveys_users.count
-  denominator == 0 ? '0 %' : ('%.2f' % (numerator*100/denominator)) + "%"
+  denominator == 0 ? 0 : '%.2f' % (numerator*100/denominator)
 end
